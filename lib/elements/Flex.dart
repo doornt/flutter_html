88f5508a90722e5414977674841f3425f60681b2
Sampson <you@example.com>
2019-02-16 18:15:41 +0800

    parse property

--- lib/elements/Flex.dart
+++ lib/elements/Flex.dart
@@ -13,15 +13,26 @@ class AttrProperty{
 
 }
 
+bool isNumeric(String s) {
+  if(s == null) {
+    return false;
+  }
+  return double.parse(s) != null;
+}
+
 class FlexElement{
 
   static Map<String,AttrProperty> attrArrayToMap(List<AttrModel> attrs,Map<String ,dynamic> params){
     Map<String,AttrProperty> map = {};
     attrs.forEach((AttrModel attr){
-      if(attr.val.startsWith("\"")){
-        map[attr.name] = AttrProperty(false, attr.val.replaceAll("\"", ""));
+      if(attr.val.length > 1 && attr.val[0] == "\"" &&  attr.val[attr.val.length - 1] == "\""){
+        map[attr.name] = AttrProperty(false, attr.val.substring(1,attr.val.length -1));
       }else if(params[attr.val] != null){
-        map[attr.name] = AttrProperty(true, params[attr.val]);
+        if(isNumeric(params[attr.val])){
+          map[attr.name] = AttrProperty(true, double.parse(attr.val));
+        }else{
+          map[attr.name] = AttrProperty(true, params[attr.val]);
+        }
       }else{
         map[attr.name] = AttrProperty(false, attr.val);
       }
